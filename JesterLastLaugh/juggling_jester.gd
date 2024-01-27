bf5ed0ff3f1f59a04536e66ecbe419e8aebfcfed
Iarzuaga <iarzuaga.developer@gmail.com>
2024-01-27 13:45:36 -0300

    Juggling first commit

---
+++ JesterLastLaugh/juggling_jester.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+@export var speed = 400
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	if velocity.x != 0:
+		#$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	move_and_slide()
