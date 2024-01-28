2b16576493a268326dd6c9b7dec4f7ea927fd971
Nestoter <nestor_e89@hotmail.com>
2024-01-28 15:25:36 -0300

    subo fruit ninja

--- JesterLastLaugh/FruitNinja/Scripts/knife.gd
+++ JesterLastLaugh/FruitNinja/Scripts/knife.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 @onready var animation = $AnimationPlayer
+@onready var sound_knife = $AudioStreamPlayer
+@onready var mini_game_manager = get_parent().get_parent()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,16 +16,22 @@ func object_type():
 	return "knife"
 	
 func start_movement(dir:Vector2,force):
+	sound_knife.play()
 	position = Vector2(position.x,position.y)
 	apply_impulse(Vector2(force*dir.x,force*dir.y),position)
 	animation.play("spin")
 
-
 func _on_body_entered(body):
 	print("KnifeContact")
-	if body.get_name() == "Floor":
+	var array_layers = mini_game_manager.to_binary(body.get_collision_layer())
+	var colission_with_floor = mini_game_manager.some_element_in_A_is_in_B(array_layers,mini_game_manager.layers_floor)
+	var olission_with_dish = mini_game_manager.some_element_in_A_is_in_B(array_layers,mini_game_manager.layers_dish)
+	if colission_with_floor:
+		print("destroy knife")
+		#If knife touhes floor, delete the knife
 		#animation.play("floor")
 		queue_free()
-	elif body.get_name() != "WallR" && body.get_name() != "WallL" && body.object_type() == "plato":
+	if olission_with_dish:
+		#If knife touhes dish, delete the dish
+		print("destroy dish")
 		body.destroy()
-	#elif body.get_name() == "Floor1" || body.get_name() == "Floor2":
