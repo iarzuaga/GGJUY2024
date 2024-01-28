3b090ad8c0cbc1a99ef5ed788c945c57edd2541c
Daril Rodriguez <darilrodriguez.2@gmail.com>
2024-01-28 15:59:05 -0300

    Lose scene

--- JesterLastLaugh/menu/scripts/game_manager.gd
+++ JesterLastLaugh/menu/scripts/game_manager.gd
@@ -6,6 +6,7 @@ var gold: int = 0
 var is_game_winned: bool = false
 var is_game_ended: bool = false
 var current_game_node: Node = null
+var guill_node: Node = null
 var is_paused: bool = true
 var timer_to_start: float = 0
 @onready var gold_counter = $Camera2D/Control/GoldCounter
@@ -14,6 +15,7 @@ var timer_to_start: float = 0
 @onready var fade = $ScreenFade
 @onready var timer = $Timer
 @onready var label = $PauseBg/PauseMenu/Label
+@onready var guillotine_scene = preload("res://guillotine/guillotine.tscn")
 @onready var games = [
 	preload("res://laudgame/note_game.tscn"),
 	preload("res://juggling/scene/juggling.tscn"),
@@ -22,6 +24,7 @@ var timer_to_start: float = 0
 ]
 var played = []
 var index = 0
+var try_count = 0
 
 func _ready():
 	gold_counter.set_gold(0)
@@ -66,7 +69,18 @@ func _process(delta: float):
 	if timer_to_start <= 0 and is_paused:
 		pause_ui.hide()
 		is_paused = false
-		current_game_node.start()
+		if current_game_node:
+			current_game_node.start()
+		if guill_node:
+			guill_node.start()
+			call_in(king_happy, 1.0)
+
+func king_happy():
+	king.set_state(1)
+	call_in(redirect_menu, 6)
+	
+func redirect_menu():
+	get_tree().change_scene_to_file('res://menu/scenes/menu.tscn')
 	
 func add_gold(amount: int):
 	gold += amount
@@ -77,16 +91,41 @@ func end_game(winned: bool):
 	is_game_ended = true
 	
 	var state: int = -1
+	king.set_state(state)
+	
 	if is_game_winned:
 		state = 1
-	
-	king.set_state(state)
+	else:
+		try_count += 1
+		if try_count >= 2:
+			call_in(start_guillotine, 5.0)
+			return
 	
 	call_in(start_change_scene, 3.0)
 
+func start_guillotine():
+	fade.fade(instance_guillotine)
+	
+func instance_guillotine():
+	if current_game_node:
+		remove_child(current_game_node)
+	
+	king.set_state(0)
+	timer.stop()
+	is_game_ended = false
+	is_game_winned = false
+	is_paused = true
+	pause_ui.show()
+	var game: PackedScene = games[index]
+	var node: Node = guillotine_scene.instantiate()
+	add_child(node)
+	current_game_node = null
+	guill_node = node
+	timer_to_start = time_to_start
+	fade.unfade()
+
 func start_change_scene():
-	fade.fade()
-	call_in(change_scene, 2.0)
+	fade.fade(change_scene)
 	
 func change_scene():
 	play_game()
--- JesterLastLaugh/menu/scripts/screen_fade.gd
+++ JesterLastLaugh/menu/scripts/screen_fade.gd
@@ -2,6 +2,8 @@ extends ColorRect
 
 var is_showed: bool = false
 var alpha: float = 0
+var callback = null
+var called = false
 
 func _ready():
 	pass
@@ -9,15 +11,27 @@ func _ready():
 func _process(delta):
 	if is_showed:
 		alpha += delta
-		if alpha > 1: alpha = 1
+		if alpha > 1: 
+			alpha = 1
+			if callback and not called:
+				callback.call()
+				called = true
 	else:
 		alpha -= delta
-		if alpha < 0: alpha = 0
+		if alpha < 0:
+			alpha = 0
+			if callback and not called:
+				callback.call()
+				called = true
 	
 	color = Color(0.0, 0.0, 0.0, alpha)
 
-func fade():
+func fade(f = null):
 	is_showed = true
+	called = false
+	callback = f
 	
-func unfade():
+func unfade(f = null):
 	is_showed = false
+	called = false
+	callback = f
